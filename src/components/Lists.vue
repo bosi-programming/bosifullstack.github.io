5c354f649a5ea2853953182fc07746ab1836ee0d
Felipe <bosifullstack@gmail.com>
2020-02-20 20:37:22 -0300

    Criação dos componentes de Lists e Message

---
+++ src/components/Lists.vue
@@ -0,0 +1,37 @@
+<template>
+  <b-container>
+    <div class="lists">
+    <ul class="nes-list is-circle">
+      <li class="lists__point"><a class="lists__link" href="https://github.com/bosifullstack/startpage">StartPage</a></li>
+      <p class="lists__text"> A PWA StartPage made with HTML, CSS and JS</p>
+      <li>Thou hast had a good night's sleep, I hope.</li>
+      <li>Thou hast had a good afternoon</li>
+      <li>Good night.</li>
+    </ul>
+  </div>
+  </b-container>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Lists extends Vue {
+
+}
+</script>
+
+<style lang="scss">
+.lists {
+  &__point {
+    text-align: left;
+  }
+  &__link {
+    color: #42b983;
+    &:hover {
+      color: #357a5a;
+      text-decoration: none;
+    }
+  }
+}
+</style>
---
+++ src/components/Message.vue
@@ -0,0 +1,33 @@
+<template>
+  <b-container>
+    <div class="nes-container is-dark message" :class="withTitle">
+      <p class="title message__title">{{ title }}</p>
+      <p class="message__text">{{ text }}</p>
+    </div>
+  </b-container>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Message extends Vue {
+  @Prop(String) private title!: string;
+  @Prop(String) private text!: string;
+
+  private withTitle: string = (this.title !== '') ? 'with-title' : '';
+}
+</script>
+
+<style lang="scss">
+.message {
+  &__title {
+    @media (min-width: 768px) {
+      font-size: 24px !important;
+    }
+  }
+  &__text {
+    text-align: left;
+  }
+}
+</style>
