a9126d97f28ab94986fa9d6ef00e34bfdf86fe15
Felipe <bosifullstack@gmail.com>
2020-03-31 15:57:55 -0300

    Making links open in another tab

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,19 +5,19 @@
       <b-row>
 
         <b-col md="3" cols="6">
-          <a href="https://github.com/bosifullstack"><i class="nes-icon github is-large" title="GitHub"></i></a>
+          <a href="https://github.com/bosifullstack" rel="noopener noreferrer" target="_blank"><i class="nes-icon github is-large" title="GitHub"></i></a>
         </b-col>
 
         <b-col md="3" cols="6">
-          <a href="https://www.linkedin.com/in/felipebosi/"><i class="nes-icon linkedin is-large" title="LinkedIn"></i></a>
+          <a href="https://www.linkedin.com/in/felipebosi/" rel="noopener noreferrer" target="_blank"><i class="nes-icon linkedin is-large" title="LinkedIn"></i></a>
         </b-col>
 
         <b-col md="3" cols="6">
-          <a href="https://twitter.com/bosiarquitetura"><i class="nes-icon twitter is-large" title="Twitter"></i></a>
+          <a href="https://twitter.com/bosiarquitetura" rel="noopener noreferrer" target="_blank"><i class="nes-icon twitter is-large" title="Twitter"></i></a>
         </b-col>
 
         <b-col md="3" cols="6">
-          <a href="mailto:bosifullstack@gmail.com"><i class="nes-icon gmail is-large" title="Email"></i></a>
+          <a href="mailto:bosifullstack@gmail.com" rel="noopener noreferrer" target="_blank"><i class="nes-icon gmail is-large" title="Email"></i></a>
         </b-col>
 
       </b-row>
