b96c30e846c586d4b949570acc6d07ed385ca672
Felipe Azevedo Bosi <felipeb@lumahealth.io>
2020-12-07 08:56:34 -0300

    Start Svelte version

---
+++ src/components/atoms/Message.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  export let title: string;
+  export let text: string;
+</script>
+
+<style>
+  .message__text {
+    text-align: left;
+  }
+  @media (min-width: 768px) {
+    .message__title {
+      font-size: 24px !important;
+    }
+  }
+</style>
+
+<div class="nes-container is-dark message {title && 'with-title'}">
+  <p class="title message__title">{title}</p>
+  <p class="message__text">{text}</p>
+</div>
